c1f1b1a2925210c45f7c0af0f7aa6dff05a87d30
Talyra42 <talyra42@foxmail.com>
2026-06-14 10:51:47 +0800

    feat: 清除冗余代码

--- frontend/tui/ink-test/source/app.tsx
+++ frontend/tui/ink-test/source/app.tsx
@@ -1,40 +1,24 @@
-import React, {FC, useState} from 'react';
-import {Box, Text, useFocus, useInput} from 'ink';
-
-const TextField: FC<{label: string}> = ({label}) => {
-  const {isFocused} = useFocus();
-  const [value, setValue] = useState('');
-
-  useInput(
-    (input, key) => {
-      if (key.backspace || key.delete) {
-        setValue(prev => prev.slice(0, -1));
-      } else if (input && !key.ctrl && !key.meta) {
-        setValue(prev => prev + input);
-      }
-    },
-    {
-      isActive: isFocused,
-    },
-  );
-
-  return (
-    <Box>
-      <Text color={isFocused ? 'green' : undefined}>
-        {label}: {value}
-        {isFocused ? '|' : ''}
-      </Text>
-    </Box>
-  );
-};
+import {Box, Spacer, Text} from 'ink';
+import React, {useEffect, useState} from 'react';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setCounter(prev => prev + 1);
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <Box borderStyle={'round'} flexDirection="column" width={35}>
-      <Text dimColor>用 Tab 切换输入框。</Text>
-      <TextField label="用户名" />
-      <TextField label="密码" />
-      <TextField label="邮箱" />
-    </Box>
+    <>
+      <Box borderStyle={'round'} width={20}>
+        <Text>Counter</Text>
+        <Spacer />
+        <Text>{counter}</Text>
+      </Box>
+    </>
   );
 }
--- frontend/tui/ink-test/source/cli.tsx
+++ frontend/tui/ink-test/source/cli.tsx
@@ -3,11 +3,4 @@ import React from 'react';
 import {render} from 'ink';
 import App from './app.js';
 
-const {waitUntilExit} = render(<App />);
-
-try {
-  await waitUntilExit();
-} catch (err) {
-  console.error(err);
-  process.exitCode = 1;
-}
+render(<App />);
