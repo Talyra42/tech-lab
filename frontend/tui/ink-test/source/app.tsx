b23a6717f6caa6eaffbac8c862d2090d827ee84d
Talyra42 <talyra42@foxmail.com>
2026-06-18 16:11:04 +0800

    feat: 实现了选择的功能

--- frontend/tui/ink-test/source/app.tsx
+++ frontend/tui/ink-test/source/app.tsx
@@ -1,10 +1,36 @@
-import {Box, Text} from 'ink';
-import React from 'react';
+import {Box, Text, useApp, useInput} from 'ink';
+import React, {useState} from 'react';
+
+const CHOICES = ['苹果', '香蕉', '橙子'];
 
 export default function App() {
+  const [choice, setChoice] = useState(0);
+  const [isSelected, setIsSelected] = useState(false);
+  const {exit} = useApp();
+
+  useInput((_, key) => {
+    if (key.upArrow) {
+      setChoice(prev => (prev > 0 ? prev - 1 : 0));
+    } else if (key.downArrow) {
+      setChoice(prev => (prev < CHOICES.length - 1 ? prev + 1 : prev));
+    } else if (key.return) {
+      setIsSelected(true);
+      setTimeout(() => {
+        exit();
+      }, 0);
+    }
+  });
+
   return (
-    <Box>
-      <Text>Hello World</Text>
+    <Box flexDirection="column">
+      <Text>请选择一个选项</Text>
+      {CHOICES.map((choiceText, index) => (
+        <Text key={choiceText} color={choice === index ? 'green' : undefined}>
+          {choice === index ? '> ' : '  '}
+          {choiceText}
+        </Text>
+      ))}
+      {isSelected && <Text color={'yellow'}>你选择了：{CHOICES[choice]}</Text>}
     </Box>
   );
 }
