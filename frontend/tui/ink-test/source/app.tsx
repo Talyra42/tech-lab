f039d01a4b190bff19217f6b7e5e3d42bc49d1ca
Talyra42 <talyra42@foxmail.com>
2026-06-18 15:52:13 +0800

    chore: 移除无用的测试文件

--- frontend/tui/ink-test/source/app.tsx
+++ frontend/tui/ink-test/source/app.tsx
@@ -1,31 +1,10 @@
-import {Box, Spacer, Text} from 'ink';
-import React, {useEffect, useState} from 'react';
-import {TextInput} from '@inkjs/ui';
+import {Box, Text} from 'ink';
+import React from 'react';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    let timer = setInterval(() => {
-      setCounter(prev => prev + 1);
-    }, 100);
-
-    return () => clearInterval(timer);
-  }, []);
-
   return (
-    <>
-      <Box flexDirection="column" borderStyle={'round'} gap={2}>
-        <Box width={20}>
-          <Text>Counter</Text>
-          <Spacer />
-          <Text>{counter}</Text>
-        </Box>
-        <Box flexDirection="column">
-          <Text>模拟登录表单</Text>
-          <TextInput placeholder="输入用户名"></TextInput>
-        </Box>
-      </Box>
-    </>
+    <Box>
+      <Text>Hello World</Text>
+    </Box>
   );
 }
