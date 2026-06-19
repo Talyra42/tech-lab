88060389d536abf7031f20021f12346392df9edb
Talyra42 <talyra42@foxmail.com>
2026-06-19 22:15:28 +0800

    feat: 实现了异步搜索的功能

--- backend/python/github-tui-test/App.py
+++ backend/python/github-tui-test/App.py
@@ -1,8 +1,11 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, DataTable
 from textual.containers import Vertical
-from textual import on
+from textual import on, work
 from SearchBar import SearchBar
+import httpx
+
+BASE_URL = "https://api.github.com/search/repositories"
 
 
 class MyApp(App):
@@ -28,7 +31,35 @@ class MyApp(App):
 
     @on(SearchBar.Submitted)
     def handle_search(self, e: SearchBar.Submitted):
-        self.notify(e.value)
+        self.search_repos(e.value)
+
+    # 专门的搜索方法，通过 work 驱动
+    @work(exclusive=True)
+    async def search_repos(self, keyword: str):
+        # 获取表格 方便后续操作
+        tb = self.query_one("#res_table", DataTable)
+
+        try:
+            async with httpx.AsyncClient() as client:
+                # 获取数据
+                response = await client.get(
+                    BASE_URL, params={"q": keyword, "per_page": 20}
+                )
+                res = response.json()["items"]
+
+                # 清空表格
+                tb.clear()
+
+                # 放入数据
+                for i in res:
+                    tb.add_row(
+                        i["full_name"],
+                        i["stargazers_count"],
+                        i["forks_count"],
+                        i["description"] or "",
+                    )
+        except Exception as err:
+            self.notify(f"搜索失败：{err}", severity="error")
 
 
 if __name__ == "__main__":
